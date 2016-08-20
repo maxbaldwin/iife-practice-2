74a1c2d43730215539c06261937f14c74f84e89e
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-20 10:08:31 -0500

    taking a 10 minute break

--- Sedan-Augmented.js
+++ Sedan-Augmented.js
@@ -2,15 +2,27 @@
 var Sedan = (function(newSedan){
   var max_occupancy = 6; 
   var current_occupancy = 0; 
+//variable below will overwrite one on Sedan.js
+  var type = "minivan";
 
   newSedan.getOccupancy = function(){
     return current_occupancy; 
   }; 
 
   newSedan.setOccupancy = function(occupancy){
-    current_occupancy = occupancy;
+ // <= translates to less than or equal to   
+    if(occupancy <= max_occupancy){
+           current_occupancy = occupancy;
+     } else {
+      //you can replace "alert" with "throw" and get rid of quotes, alerts error in DOM
+      alert("Cannot exceed maximum occupancy of " + max_occupancy + "!");
+     }
+  };
+//now you are overwriting what is on Sedan.js
+  newSedan.getType = function(){
+    return "minivan"; 
   }
-
+//calling what you created above
   return newSedan;
 
 })(Sedan || {});
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-//Why is this last? Anything you want the other JS files to do and this returns to the DOM
+//Why is this last on HTML? Anything you want the other JS files to do and this returns to the DOM
 console.log("Sedan IIFE", Sedan); 
 console.log("Sedan Color", Sedan.getColor()); 
 console.log("Sedan Type", Sedan.getType()); 
-console.log("Sedan Set Occupany", Sedan.setOccupancy(4));
+//setting the occupancy on line 6, so you can see it on line 7
+console.log("Sedan Set Occupany", Sedan.setOccupancy(7));
 console.log("Sedan Set Occupany", Sedan.getOccupancy());
